ed58bfa263a74f76fb05bec13214e60aaee99d79
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2025-11-01 11:30:39 +0100

    initial commit

---
+++ Backend/dist/defaultVending.js
@@ -0,0 +1,46 @@
+// This takes up for initial view when you've not stored to the local storage
+export const defaultStock = {
+    machineId: "M001",
+    slots: [
+        {
+            slotCode: "A1", product: { p_id: "COKE-500", name: "Coca-Cola", price: 1000, imageURL: "../Backend/images/Coca-cola.png", category: "Drink", size: "500ml" },
+            quantity: 2,
+            capacity: 10,
+            lastRestockedAt: new Date().toISOString(),
+            soldCount: 0,
+            status: "ACTIVE"
+        },
+        {
+            slotCode: "A2", product: { p_id: "PEPSI-500", name: "Pepsi", price: 900, imageURL: "../Backend/images/Pepsi.png", category: "Drink", size: "500ml" },
+            quantity: 5,
+            capacity: 10,
+            lastRestockedAt: new Date().toISOString(),
+            soldCount: 0,
+            status: "ACTIVE"
+        },
+        {
+            slotCode: "B3", product: { p_id: "Yoghurt-500ml", name: "Yoghurt", price: 900, imageURL: "../Backend/images/Yoghurt.png", category: "Drink", size: "500ml" },
+            quantity: 5,
+            capacity: 10,
+            lastRestockedAt: new Date().toISOString(),
+            soldCount: 0,
+            status: "ACTIVE"
+        },
+        {
+            slotCode: "B5", product: { p_id: "Water-50CL", name: "BottleWater", price: 500, imageURL: "../Backend/images/Bottle-Water.png", category: "Snack", size: "50g" },
+            quantity: 8,
+            capacity: 10,
+            lastRestockedAt: new Date().toISOString(),
+            soldCount: 0,
+            status: "ACTIVE"
+        },
+        {
+            slotCode: "C2", product: { p_id: "CHIPS-50G", name: "Plantain Chips", price: 500, imageURL: "../Backend/images/Plantain-Chips.png", category: "Snack", size: "50g" },
+            quantity: 5,
+            capacity: 10,
+            lastRestockedAt: new Date().toISOString(),
+            soldCount: 0,
+            status: "ACTIVE"
+        }
+    ]
+};
