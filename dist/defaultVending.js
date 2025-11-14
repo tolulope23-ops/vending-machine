b36e963acf95070462cbc9cabd4338d4d50699bb
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2025-11-14 10:29:58 +0100

    refactor: product image

--- dist/defaultVending.js
+++ dist/defaultVending.js
@@ -3,7 +3,7 @@ export const defaultStock = {
     machineId: "M001",
     slots: [
         {
-            slotCode: "A1", product: { p_id: "COKE-500", name: "Coca-Cola", price: 1000, imageURL: "../Backend/images/Coca-cola.png", category: "Drink", size: "500ml" },
+            slotCode: "A1", product: { p_id: "COKE-500", name: "Coca-Cola", price: 1000, imageURL: "./images/Coca-cola.png", category: "Drink", size: "500ml" },
             quantity: 2,
             capacity: 10,
             lastRestockedAt: new Date().toISOString(),
@@ -11,7 +11,7 @@ export const defaultStock = {
             status: "ACTIVE"
         },
         {
-            slotCode: "A2", product: { p_id: "PEPSI-500", name: "Pepsi", price: 900, imageURL: "../Backend/images/Pepsi.png", category: "Drink", size: "500ml" },
+            slotCode: "A2", product: { p_id: "PEPSI-500", name: "Pepsi", price: 900, imageURL: "./images/Pepsi.png", category: "Drink", size: "500ml" },
             quantity: 5,
             capacity: 10,
             lastRestockedAt: new Date().toISOString(),
@@ -19,7 +19,7 @@ export const defaultStock = {
             status: "ACTIVE"
         },
         {
-            slotCode: "B3", product: { p_id: "Yoghurt-500ml", name: "Yoghurt", price: 900, imageURL: "../Backend/images/Yoghurt.png", category: "Drink", size: "500ml" },
+            slotCode: "B3", product: { p_id: "Yoghurt-500ml", name: "Yoghurt", price: 900, imageURL: "./images/Yoghurt.png", category: "Drink", size: "500ml" },
             quantity: 5,
             capacity: 10,
             lastRestockedAt: new Date().toISOString(),
@@ -27,7 +27,7 @@ export const defaultStock = {
             status: "ACTIVE"
         },
         {
-            slotCode: "B5", product: { p_id: "Water-50CL", name: "BottleWater", price: 500, imageURL: "../Backend/images/Bottle-Water.png", category: "Snack", size: "50g" },
+            slotCode: "B5", product: { p_id: "Water-50CL", name: "BottleWater", price: 500, imageURL: "./images/Bottle-Water.png", category: "Snack", size: "50g" },
             quantity: 8,
             capacity: 10,
             lastRestockedAt: new Date().toISOString(),
@@ -35,7 +35,7 @@ export const defaultStock = {
             status: "ACTIVE"
         },
         {
-            slotCode: "C2", product: { p_id: "CHIPS-50G", name: "Plantain Chips", price: 500, imageURL: "../Backend/images/Plantain-Chips.png", category: "Snack", size: "50g" },
+            slotCode: "C2", product: { p_id: "CHIPS-50G", name: "Plantain Chips", price: 500, imageURL: "./images/Plantain-Chips.png", category: "Snack", size: "50g" },
             quantity: 5,
             capacity: 10,
             lastRestockedAt: new Date().toISOString(),
